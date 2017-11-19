1538df56f0002c0c87492427115a4b9e61f726d7
roman <s91630277@gmail.com>
2017-11-19 17:56:50 +0400

    Do checkout before switch

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,15 +1,18 @@
 import { pspawn } from './pspawn';
 import { promisify } from 'util';
 import { existsSync } from 'fs';
+import { SpawnOptions } from 'child_process';
 
 export async function sandbox(n: number, revision: string) {
     const directory = `~benchmarko.sandbox/${n}`;
+    const spawnOptions: SpawnOptions = { stdio: 'inherit' };
     if (!existsSync(directory)) {
         await pspawn('git', ['clone', '.', directory]);
     } else {
         await pspawn('git', ['pull']);
     }
     const cwd = `${process.cwd()}/${directory}`;
-    await pspawn('git', ['checkout', '-', revision], { cwd });
+    await pspawn('git', ['checkout', '-f', 'master'], { ...spawnOptions, cwd });
+    await pspawn('git', ['checkout', '-f', revision], { ...spawnOptions, cwd });
     return { directory: cwd, revision };
 }
